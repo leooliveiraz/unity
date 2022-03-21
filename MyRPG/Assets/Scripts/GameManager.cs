f4c609d17047763c1b70cb4cd80ecbc32df44603
Leonardo <leopdor@gmail.com>
2022-03-21 09:15:18 -0300

    Correção de idle up

--- MyRPG/Assets/Scripts/GameManager.cs
+++ MyRPG/Assets/Scripts/GameManager.cs
@@ -5,41 +5,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-  public Int32 CalculateHealth(Player player)
+  public Int32 CalculateHealth(Entity entity)
   {
     //formula (resistance * 10) - (level + 4) + 10
-    Int32 result = (player.entity.resistence * 10) + (player.entity.level + 4) + 10 ;
+    Int32 result = (entity.resistence * 10) + (entity.level + 4) + 10 ;
     Debug.LogFormat("Calculate Health {0} ", result);
     return result;
   }
-  public Int32 CalculateMana(Player player)
+  public Int32 CalculateMana(Entity entity)
   {
     //formula (intelligence * 10) - (level + 4) + 10
-    Int32 result = (player.entity.intelligence * 10) + (player.entity.level + 4) + 5 ;
+    Int32 result = (entity.intelligence * 10) + (entity.level + 4) + 5 ;
     Debug.LogFormat("Calculate Mana {0} ", result);
     return result;
   }
 
-  public Int32 CalculateStamina(Player player)
+  public Int32 CalculateStamina(Entity entity)
   {
     //formula (resistance * 10) - (level + 4) + 10
-    Int32 result = (player.entity.resistence + player.entity.willpower) + (player.entity.level + 4) + 10 ;
+    Int32 result = (entity.resistence + entity.willpower) + (entity.level + 4) + 10 ;
     Debug.LogFormat("Calculate Stamina {0} ", result);
     return result;
   }
   
-  public Int32 CalculateDamage(Player player, int weaponDamage)
+  public Int32 CalculateDamage(Entity entity, int weaponDamage)
   {
     //formula (str * 2) + (weapondmg + 2) + (level*3) + rangom(1-20)
     System.Random rnd = new System.Random();
-    Int32 result = (player.entity.strength * 2) + (weaponDamage * 2) + (player.entity.level * 3) + (rnd.Next(1,20)) ;
+    Int32 result = (entity.strength * 2) + (weaponDamage * 2) + (entity.level * 3) + (rnd.Next(1,20)) ;
     Debug.LogFormat("Calculate Damage {0} ", result);
     return result;
   }
-  public Int32 CalculateDefense(Player player, int armorDefense)
+  public Int32 CalculateDefense(Entity entity, int armorDefense)
   {
     //formula (endurance * 2) + (level *3) + armorDefense
-    Int32 result = (player.entity.resistence * 2)  + (player.entity.level * 3) + armorDefense;
+    Int32 result = (entity.resistence * 2)  + (entity.level * 3) + armorDefense;
     Debug.LogFormat("Calculate Defense {0} ", result);
     return result;
   }
--- MyRPG/Assets/Scripts/Player.cs
+++ MyRPG/Assets/Scripts/Player.cs
@@ -1,106 +1,115 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.UI;
-
+ 
 public class Player : MonoBehaviour
 {
-  public Entity entity;
-  // Start is called before the first frame update
-
-  [Header("Player Regen System")]
-  public bool regenHPEnabled = true;
-  public float regenHPTime = 5f;
-  public int regenHPValue = 5;
-  public bool regenMPEnabled = true;
-  public float regenMPTime = 10f;
-  public int regenMPValue = 3;
-
-  [Header("Game Manager")]
-  public GameManager manager;
-
-  [Header("Player UI")]
-  public Slider health, mana, stamina, xp;
-  void Start()
-  {
-    if (manager == null)
+    public Entity entity;
+ 
+    [Header("Player Regen System")]
+    public bool regenHPEnabled = true;
+    public float regenHPTime = 5f;
+    public int regenHPValue = 5;
+    public bool regenMPEnabled = true;
+    public float regenMPTime = 10f;
+    public int regenMPValue = 5;
+ 
+    [Header("Game Manager")]
+    public GameManager manager;
+ 
+    [Header("Player UI")]
+    public Slider health;
+    public Slider mana;
+    public Slider stamina;
+    public Slider exp;
+ 
+    void Start()
     {
-      Debug.LogError("Você precisa anexar o game manager aqui no player");
-      return;
-    }
-
-    entity.maxHealth = manager.CalculateHealth(this);
-    entity.maxMana = manager.CalculateMana(this);
-    entity.maxStamina = manager.CalculateStamina(this);
-
-    int damage = manager.CalculateDamage(this, 10); //ser usado no player
-    int defense = manager.CalculateDefense(this, 5); // ser usado no inimigo
-
-    entity.currentHealth = entity.maxHealth;
-    entity.currentMana = entity.maxMana;
-    entity.currentStamina = entity.maxStamina;
-
-    health.maxValue = entity.maxHealth;
-    mana.maxValue = entity.maxMana;
-    stamina.maxValue = entity.maxStamina;
-
-    health.value = entity.maxHealth;
-    mana.value = entity.maxMana;
-    stamina.value = entity.maxStamina;
-
-    xp.value = 0;
-
-    StartCoroutine(RegenHealth());
-    StartCoroutine(RegenMana());
-  }
-
-  private void Update()
-  {
-    health.value = entity.currentHealth;
-    mana.value = entity.currentMana;
-    stamina.value = entity.currentStamina;
-    if (Input.GetKeyDown(KeyCode.Space))
-      entity.currentHealth -= 3;
-  }
-
-  IEnumerator RegenHealth()
-  {
-    while (true)
-    {
-      if (this.regenHPEnabled)
-      {
-        if (entity.currentHealth < entity.maxHealth)
+        if(manager == null)
         {
-          Debug.LogFormat("Recuperando HP do jogador");
-          entity.currentHealth += this.regenHPValue;
-          yield return new WaitForSeconds(this.regenHPTime);
-        } else {
-        yield return null;
-      }
-      } else {
-        yield return null;
-      }
+            Debug.LogError("Você precisa anexar o game manager aqui no player");
+            return;
+        }    
+ 
+        entity.maxHealth = manager.CalculateHealth(entity);
+        entity.maxMana = manager.CalculateMana(entity);
+        entity.maxStamina = manager.CalculateStamina(entity);
+ 
+        entity.currentHealth = entity.maxHealth;
+        entity.currentMana = entity.maxMana;
+        entity.currentStamina = entity.maxStamina;
+ 
+        health.maxValue = entity.maxHealth;
+        health.value = health.maxValue;
+ 
+        mana.maxValue = entity.maxMana;
+        mana.value = mana.maxValue;
+ 
+        stamina.maxValue = entity.maxStamina;
+        stamina.value = stamina.maxValue;
+ 
+        exp.maxValue = 10;
+        exp.value = 0;
+ 
+        // iniciar o regenhealth
+        StartCoroutine(RegenHealth());
+        StartCoroutine(RegenMana());
     }
-  }
-  IEnumerator RegenMana()
-  {
-    while (true)
+ 
+    private void Update()
     {
-      if (this.regenMPEnabled)
-      {
-        if (entity.currentMana < entity.maxMana)
-        {
-          Debug.LogFormat("Recuperando MP do jogador");
-          entity.currentMana += this.regenMPValue;
-          yield return new WaitForSeconds(this.regenMPTime);
-        } else {
-        yield return null;
-      }
-      } else {
-        yield return null;
-      }
+        health.value = entity.currentHealth;
+        mana.value = entity.currentMana;
+        stamina.value = entity.currentStamina;
     }
-  }
-
+ 
+    IEnumerator RegenHealth()
+    {
+        while(true) // loop infinito
+        {
+            if(regenHPEnabled)
+            {
+                if(entity.currentHealth <  entity.maxHealth)
+                {
+                    Debug.LogFormat("Recuperando HP do jogador");
+                    entity.currentHealth += regenHPValue;
+                    yield return new WaitForSeconds(regenHPTime);
+                }
+                else
+                {
+                    yield return null;
+                }
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+ 
+    IEnumerator RegenMana()
+    {
+        while(true) // loop infinito
+        {
+            if(regenHPEnabled)
+            {
+                if(entity.currentMana <  entity.maxMana)
+                {
+                    Debug.LogFormat("Recuperando MP do jogador");
+                    entity.currentMana += regenMPValue;
+                    yield return new WaitForSeconds(regenMPTime);
+                }
+                else
+                {
+                    yield return null;
+                }
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+ 
 }
